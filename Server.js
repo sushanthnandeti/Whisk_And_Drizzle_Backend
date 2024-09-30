0a7d97ae90368e98dd36e7229c2469e2da87c54a
Sushanth Nandeti <sushanthnandeti96@gmail.com>
2024-09-30 02:46:39 -0400

    Signup and Logout functionalities added

--- Server.js
+++ Server.js
@@ -1,12 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
 import {connectDB} from "./lib/db.js";
 
 const app = express();
 app.use(express.json());
-
+app.use(cookieParser());
 
 dotenv.config();
  
--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -1,6 +1,6 @@
 import User from "../models/user.model.js";
 import jwt from "jsonwebtoken";
-import redis from "ioredis";
+import {redis} from "../lib/redis.js";
 
 
 const generateTokens = (userId) => {
@@ -15,7 +15,7 @@ const generateTokens = (userId) => {
     return {accessToken, refreshToken}
 }
 
-const storeRefreshTokens = async(userId, refreshToken)  => {
+const storeRefreshToken = async(userId, refreshToken)  => {
    await redis.set(`refresh_token: ${userId}`, refreshToken, "EX", 7*24*60*60); // 7 DAYS
 }
 
@@ -40,6 +40,8 @@ const setCookies = (res, accessToken, refreshToken) => {
     }
 
 
+// Signup Logic
+
 export const signup =  async (req,res) => {
     const { email, password, name} = req.body;
     
@@ -53,7 +55,7 @@ export const signup =  async (req,res) => {
 
     //authenticate
     const {accessToken, refreshToken} = generateTokens(user._id)
-    await storeRefreshTokens(user._id, refreshToken)
+    await storeRefreshToken(user._id, refreshToken)
     setCookies(res, accessToken, refreshToken);
 
     res.status(201).json({ user : {
@@ -68,9 +70,53 @@ export const signup =  async (req,res) => {
     }
 };
 
+
+// Login Logic
 export const login =  async (req,res) => {
-    res.send("login up Called");
+    
+    try {
+        
+        const {email, password} = req.body;
+        const user = await User.findOne({email});
+        
+        console.log("test1");
+
+        if (user && (await user.comparePassword(password))) {
+            console.log("test2");
+            const { accessToken, refreshToken} = generateTokens(user._id);
+            await storeRefreshToken(user._id, refreshToken);
+            setCookies(res, accessToken, refreshToken);
+
+            res.json({
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role,
+            });
+
+        } else{ 
+            res.status(401).json({message: "Invalid Credentials"});
+        }
+        
+    } catch (error) { 
+        console.log("Error in login", error.message);
+        res.status(500).json({message: error.message});
+    }
 }
+
+
+// Logout Logic
 export const logout =  async (req,res) => {
-    res.send("logout up Called");
+    try {
+        const refreshToken = req.cookies.refreshToken;
+        if(refreshToken) {
+            const decoded = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
+            await redis.del(`refresh_token: ${decoded.userId}`);   
+        }
+        res.clearCookie("accessToken");
+        res.clearCookie("refreshToken");
+        res.json("Logged out Successfully");
+    } catch (error) {
+        res.status(500).json({message : "Server Error", error: error_message});
+    }
 }
