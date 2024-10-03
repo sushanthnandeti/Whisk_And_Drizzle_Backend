079e00b702f767b46cae7503123ead65c00f3ee6
Sushanth Nandeti <sushanthnandeti96@gmail.com>
2024-10-03 18:42:28 -0400

    Cart controllers added

--- Server.js
+++ Server.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
+import cartRoutes from "./routes/cart.route.js";
 
 import {connectDB} from "./lib/db.js";
 
@@ -19,6 +20,7 @@ const PORT = process.env.PORT || 4000;
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
 
 
 app.listen(PORT, () => {
---
+++ routes/cart.route.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import {addToCart, removeAllFromCart, updateQuantity, getCartProducts} from "../controllers/cart.controller.js";
+
+const router = express.Router();
+
+router.get("/", protectRoute, getCartProducts );
+router.post("/", protectRoute, addToCart);
+router.delete("/", protectRoute, removeAllFromCart);
+router.put("/:id", protectRoute, updateQuantity);
+
+export default router;
