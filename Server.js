cbd485fef7eef4d64c8cabb11985493a607cbd17
Sushanth Nandeti <sushanthnandeti96@gmail.com>
2024-10-03 19:11:07 -0400

    Coupon Routes and Controllers added

--- Server.js
+++ Server.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
+import couponRoutes from "./routes/coupon.route.js";
 
 import {connectDB} from "./lib/db.js";
 
@@ -21,6 +22,7 @@ const PORT = process.env.PORT || 4000;
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
+app.use("/api/coupons", couponRoutes);
 
 
 app.listen(PORT, () => {
